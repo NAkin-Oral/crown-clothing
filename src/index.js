9387fbd1ddbf3715e28f13cb84d7f6132bf1b48f
nao <nurettinakinoral@gmail.com>
2022-07-27 19:04:27 +0300

    solwing problem

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
+// import React from 'react';
+// // import { render } from 'react-dom';
+// import './index.scss';
+// import App from './App';
+// import ReactDOM from 'react-dom/client';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
+
 import React from 'react';
-// import { render } from 'react-dom';
-import './index.scss';
-import App from './App';
 import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
